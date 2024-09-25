e913cf0f3b45953e1cf0283e1f5c09c8096f2fa5
Brooke Chalmers <breq@breq.dev>
2024-09-24 23:14:28 -0400

    Initial commit
    Co-authored-by: Ava Silver <ava@avasilver.dev>

---
+++ main.go
@@ -0,0 +1,161 @@
+package main
+
+// An example Bubble Tea server. This will put an ssh session into alt screen
+// and continually print up to date terminal information.
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/bubbles/timer"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/log"
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/activeterm"
+	"github.com/charmbracelet/wish/bubbletea"
+	"github.com/charmbracelet/wish/logging"
+)
+
+const (
+	host = "localhost"
+	port = "23234"
+)
+
+const fish = `
+                ,"(
+               ////\                           _
+              (//////--,,,,,_____            ,"
+            _;"""----/////_______;,,        //
+__________;"o,-------------......"""""`+"`"+`'-._/(
+      ""'==._.__,;;;;"""           ____,.-.==
+             "-.:______,...;---""/"   "    \(
+                 '-._      `+"`"+`-._("   ctr     \\
+                     '-._                    '._
+`
+
+func main() {
+	s, err := wish.NewServer(
+		wish.WithAddress(net.JoinHostPort(host, port)),
+		wish.WithHostKeyPath(".ssh/id_ed25519"),
+		wish.WithMiddleware(
+			bubbletea.Middleware(teaHandler),
+			activeterm.Middleware(), // Bubble Tea apps usually require a PTY.
+			logging.Middleware(),
+		),
+	)
+	if err != nil {
+		log.Error("Could not start server", "error", err)
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Info("Starting SSH server", "host", host, "port", port)
+	go func() {
+		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+			log.Error("Could not start server", "error", err)
+			done <- nil
+		}
+	}()
+
+	<-done
+	log.Info("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		log.Error("Could not stop server", "error", err)
+	}
+}
+
+// You can wire any Bubble Tea model up to the middleware with a function that
+// handles the incoming ssh.Session. Here we just grab the terminal info and
+// pass it to the new model. You can also return tea.ProgramOptions (such as
+// tea.WithAltScreen) on a session by session basis.
+func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+	// This should never fail, as we are using the activeterm middleware.
+	// pty, _, _ := s.Pty()
+	s.Pty()
+
+	// When running a Bubble Tea app over SSH, you shouldn't use the default
+	// lipgloss.NewStyle function.
+	// That function will use the color profile from the os.Stdin, which is the
+	// server, not the client.
+	// We provide a MakeRenderer function in the bubbletea middleware package,
+	// so you can easily get the correct renderer for the current session, and
+	// use it to create the styles.
+	// The recommended way to use these styles is to then pass them down to
+	// your Bubble Tea model.
+	renderer := bubbletea.MakeRenderer(s)
+	// appStyle := renderer.NewStyle().Background(lipgloss.Color("240"))
+	appStyle := renderer.NewStyle()
+	txtStyle := renderer.NewStyle().Foreground(lipgloss.Color("10")).Inherit(appStyle)
+	fishStyle := renderer.NewStyle().Foreground(lipgloss.Color("8")).Inherit(appStyle)
+	quitStyle := renderer.NewStyle().Foreground(lipgloss.Color("8")).Inherit(appStyle)
+
+	m := model{
+		timer: timer.NewWithInterval(999999999 * time.Second, time.Millisecond),
+		time:     time.Now(),
+		appStyle: appStyle,
+		txtStyle:  txtStyle,
+		fishStyle: fishStyle,
+		quitStyle: quitStyle,
+	}
+	return m, []tea.ProgramOption{tea.WithAltScreen()}
+}
+
+type model struct {
+	timer	timer.Model
+	time 	time.Time
+	width int
+	height int
+	appStyle lipgloss.Style
+	txtStyle  lipgloss.Style
+	fishStyle lipgloss.Style
+	quitStyle lipgloss.Style
+}
+
+func (m model) Init() tea.Cmd {
+	return m.timer.Init()
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+		return m, nil
+	case timer.TickMsg:
+		var cmd tea.Cmd
+		m.timer, cmd = m.timer.Update(msg)
+		m.time = time.Now()
+		return m, cmd
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	s := fmt.Sprintf("The time is %s", m.time.Format("15:04:05"))
+	if m.time.Format("15:04") == "11:11" {
+		s = fmt.Sprintf("%s\n\n%s", m.txtStyle.Render(s), m.fishStyle.Render(fish))
+	} else {
+		s = fmt.Sprintf("%s\n\n%s", m.txtStyle.Render(s), m.fishStyle.Render("Come back at 11:11"))
+	}
+	s = fmt.Sprintf("%s\n\n%s",  s, m.quitStyle.Render("Press 'q' to quit"))
+
+	// center the text
+	s = lipgloss.Place(m.width, m.height, lipgloss.Center, lipgloss.Center, s)
+
+	return m.appStyle.Copy().Width(m.width).Height(m.height).Render(s)
+}
